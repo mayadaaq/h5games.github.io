c706b9a872b8c8e434b9788c15add9060300864b
mayadaaq <112697814+mayadaaq@users.noreply.github.com>
2024-02-01 16:03:57 +0900

    Update game.js

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ class Game {
     this.score = 0;
     this.choice = '';
     this.muted = false;
+    this.shouldShowAdOnPlay = false;
 
     this.canvas = document.getElementById('gameContainer').getContext('2d');
     this.canvas.font = '24px Arial';
@@ -43,43 +44,85 @@ class Game {
     });
 
     this.erase();
+  }
+
   // Start the game
   play() {
     if (this.shouldShowAdOnPlay) {
       this.shouldShowAdOnPlay = false;
 
       adBreak({
-        type: 'next',  // ad shows at start of next level
+        type: 'next',
         name: 'restart-game',
-        beforeAd: () => { this.disableButtons(); },  // You may also want to mute the game's sound.
-        afterAd: () => { this.enableButtons(); },    // resume the game flow.
+        beforeAd: () => {
+          this.disableButtons();
+        },
+        afterAd: () => {
+          this.enableButtons();
+        },
       });
     }
 
     this.score = 0;
     this.canvas.fillText('Score: ' + this.score, 8, 26);
     this.canvas.fillText('Heads or Tails?', 66, 150);
-    this.playButton.style.display = 'none'
-    this.headsButton.style.display = 'inline-block'
-    this.tailsButton.style.display = 'inline-block'
+    this.playButton.style.display = 'none';
+    this.headsButton.style.display = 'inline-block';
+    this.tailsButton.style.display = 'inline-block';
   }
 
-  [...]
+  // Flip the coin
+  flipCoin() {
+    this.headsButton.disabled = true;
+    this.tailsButton.disabled = true;
+    this.erase();
+    this.canvas.fillText('Score: ' + this.score, 8, 26);
+    this.canvas.fillText('Flipping coin . . .', 60, 150);
+
+    setTimeout(() => {
+      this.coinLanded();
+    }, 2000);
+  }
+
+  // Logic for when the coin lands
+  coinLanded() {
+    this.headsButton.disabled = false;
+    this.tailsButton.disabled = false;
+    let sideUp;
+    if (Math.random() < 0.5) {
+      sideUp = 'Heads';
+    } else {
+      sideUp = 'Tails';
+    }
+
+    if (sideUp === this.choice) {
+      this.win(sideUp);
+    } else {
+      this.lose(sideUp);
+    }
+  }
+
+  // Guess the flip correctly
+  win(sideUp) {
+    this.erase();
+    this.score += 1;
+    this.canvas.fillText('Score: ' + this.score, 8, 26);
+    this.canvas.fillText('It was ' + sideUp + '!', 66, 150);
+    this.canvas.fillText('Guess again', 70, 200);
+  }
 
   // Guess the flip incorrectly
   lose(sideUp) {
-    this.erase()
+    this.erase();
     this.canvas.fillText('Sorry, it was ' + sideUp, 50, 100);
     this.canvas.fillText('Your score was ' + this.score, 50, 150);
     this.canvas.fillText('Want to play again?', 45, 200);
 
-    this.playButton.style.display = 'inline-block'
-    this.headsButton.style.display = 'none'
-    this.tailsButton.style.display = 'none'
+    this.playButton.style.display = 'inline-block';
+    this.headsButton.style.display = 'none';
+    this.tailsButton.style.display = 'none';
     this.shouldShowAdOnPlay = true;
   }
-
-  [...]
 
   // Erase the canvas
   erase() {
